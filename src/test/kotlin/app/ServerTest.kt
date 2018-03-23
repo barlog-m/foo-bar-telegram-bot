d12e2743cd6b420dade5c39135627ef5238920b1
Barlog <barlog@tanelorn.li>
2018-03-23 19:17:15 +0300

    Integration test fix

--- src/test/kotlin/app/ServerTest.kt
+++ src/test/kotlin/app/ServerTest.kt
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions.assertTrue
 class ServerTest {
     @Test
     fun start() {
+        appArgs = listOf()
         Server.start("localhost", 8889)
         assertTrue(Server.isStarted())
         Server.stop()
