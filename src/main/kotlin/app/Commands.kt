51461129c78f90751bc0247ae487993ba655a6b8
Barlog <barlog@tanelorn.li>
2018-03-27 18:45:54 +0300

    Fix command names

--- src/main/kotlin/app/Commands.kt
+++ src/main/kotlin/app/Commands.kt
@@ -5,7 +5,7 @@ import app.service.hiCommand
 import app.service.startCommand
 
 val commands = mapOf(
-    "/start" to ::startCommand,
-    "/help" to ::helpCommand,
-    "/hi" to ::hiCommand
+    "start" to ::startCommand,
+    "help" to ::helpCommand,
+    "hi" to ::hiCommand
 )
